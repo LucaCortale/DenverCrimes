ab295af18e97eabaa4ef44b5e8b0bf1a565bdbf4
LUCA <LUCA@LAPTOP-PQ9R8PH4>
2022-07-13 23:17:35 +0200

    aa

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -6,6 +6,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import it.polito.tdp.crimes.model.Adiacenza;
 import it.polito.tdp.crimes.model.Event;
 
 
@@ -52,6 +54,82 @@ public class EventsDao {
 			e.printStackTrace();
 			return null ;
 		}
+	}
+	
+	public List<String> getVertici(String categoria,int mese){
+		
+		String sql = "SELECT DISTINCT offense_type_id FROM events WHERE offense_type_id = ? AND MONTH(reported_date) = ? ";
+		List <String> vertici = new ArrayList<String>();
+		
+		
+		
+			try {
+				Connection conn = DBConnect.getConnection();
+				PreparedStatement st = conn.prepareStatement(sql);
+				st.setString(1, categoria);
+				st.setInt(2, mese);
+				ResultSet res = st.executeQuery();
+				
+					while(res.next()) {
+						String s = res.getString("offense_type_id");
+						vertici.add(s);
+					
+					}
+			
+		
+		
+		conn.close();
+		return vertici ;
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	
+	public List<Adiacenza> getAdiacenza(String categoria,int mese){
+		
+		String sql = "SELECT e1.offense_type_id AS v1, e2.offense_type_id AS v2, COUNT(DISTINCT e1.neighborhood_id) AS peso "
+				+ "FROM events e1, events e2 "
+				+ "WHERE e1.offense_type_id > e2.offense_type_id AND e1.offense_category_id = ? "
+				+ "AND e1.offense_category_id = e2.offense_category_id AND MONTH(e1.reported_date) = ? "
+				+ "AND MONTH(e1.reported_date) = MONTH(e2.reported_date) AND e1.neighborhood_id = e2.neighborhood_id "
+				+ "GROUP BY e1.offense_type_id, e2.offense_type_id ";
+		
+		List <Adiacenza> archi = new ArrayList<Adiacenza>();
+		
+		
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			ResultSet res = st.executeQuery();
+			
+				while(res.next()) {
+					String v1 = res.getString("v1");
+					String v2 = res.getString("v2");
+					int peso = res.getInt("peso");
+					Adiacenza a = new Adiacenza(v1,v2,peso);
+					archi.add(a);
+				
+				}
+		
+	
+	
+	conn.close();
+	return archi ;
+
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return null ;
+	}
+		
+		
 	}
 
 }
